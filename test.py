b43b2c171324d6afbda15dbec6b778261c329827
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-06-17 16:53:08 +0300

    update

---
+++ test.py
@@ -0,0 +1,11 @@
+def CoverWithTiles(len):
+    if len % 2 == 1 or len <= 0:
+        return - 1
+    if len == 2 :
+        return 3
+    elif len == 4 :
+        return 11
+    else:
+        return 4 * CoverWithTiles(len - 2) - CoverWithTiles(len - 4)
+    
+print(CoverWithTiles(12))
